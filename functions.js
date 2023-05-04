f16901d31cf5f8ce9c94708b85d1b23966253dc2
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-05-05 00:10:00 +0300

    functions

---
+++ functions.js
@@ -0,0 +1,8 @@
+// Write a function that takes a string as input and returns the number of vowels in the string.
+
+function vowels_input(){
+    let count=0
+    vowels=["a","e","i","o","u"]
+    
+
+}
