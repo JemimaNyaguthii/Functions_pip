f268a23807226077668e6d7915ca06ed601c4018
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-05-05 20:35:55 +0300

    functions

--- functions.js
+++ functions.js
@@ -1,8 +1,48 @@
 // Write a function that takes a string as input and returns the number of vowels in the string.
 
-function vowels_input(){
-    let count=0
-    vowels=["a","e","i","o","u"]
-    
+function vowels_input(word){
+    vowels=(aeiou)
+    for(let i=0;i<word.length;i++)
+    if(vowels.includes[1]){
+        return vowels
+    }
+    else{
+        
+    }
 
 }
+//  let word="beautiful"
+// console.log(vowels_input(word))
+// Write a function that takes an array of numbers as input and returns the sum of all the numbers.
+
+
+function array_nums(nums){
+     sum1=0;
+    nums.forEach( nums=> {
+        console.log(sum1+=nums)
+        
+    });
+}
+(array_nums(2,3,4,5,6,7,8))
+
+
+
+// Write a function that takes two numbers as input and returns true if their sum is greater than 100, and false otherwise.
+function two_input(num1,num2){
+    for (let num = 0;num < num.length; num++) {
+        sum=num1 + num2
+        if(sum>100 ){
+            return true
+        }
+        else{
+            return false
+        }       
+    }
+}
+console.log(two_input(45,78))
+// Write a function that takes in a array of numbers as a parameter
+//  and returns the second largest number in the array
+function second_largest(){
+
+}
+
